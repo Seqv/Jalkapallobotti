040147e7932ab9256fe28b408d58b69bc3f6adeb
Seve <36482151+Seqv@users.noreply.github.com>
2024-04-05 14:04:40 +0300

    Update orava.py

--- orava.py
+++ orava.py
@@ -4,7 +4,7 @@ from pynput import keyboard
 
 evenement=""
 print("Start")
-port="COM6" #This will be different for various devices and on windows it will probably be a COM port.
+port="COMX" #This will be different for various devices and on windows it will probably be a COM port.
 bluetooth=serial.Serial(port, 9600)#Start communications with the bluetooth unit
 print("Connected")
 bluetooth.flushInput() #This gives the bluetooth a little kick
